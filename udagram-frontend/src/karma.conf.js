cb7349f185dbc10bdb954a523b94fd929fe13462
Ahmed Abdelaal <a.abdelaal@protonmail.com>
2022-04-04 00:22:38 +0300

    try docker headless chrome

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -24,7 +24,15 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    singleRun: true
+    autoWatch: true,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: { 
+      ChromeHeadlessNoSandbox: { 
+        base: 'ChromeHeadless', 
+        flags: ['--no-sandbox',
+                '--disable-setuid-sandbox'] 
+      }
+    },
+    singleRun: false
   });
 };
